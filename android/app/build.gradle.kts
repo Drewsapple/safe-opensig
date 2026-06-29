62f7515026ff846ed4413cd1547ec72c2a34f453
Drew Fisher <Drewsapple@users.noreply.github.com>
2026-06-29 11:54:28 -0400

    chore(personal): rename to com.drewf.safeopensig and bump to 1.1.5-erc4527+10

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.candidelabs.opensig"
+    namespace = "com.drewf.safeopensig"
     compileSdk = 36
 //    compileSdk = flutter.compileSdkVersion
 //    ndkVersion = flutter.ndkVersion
@@ -45,7 +45,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.candidelabs.opensig"
+        applicationId = "com.drewf.safeopensig"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
